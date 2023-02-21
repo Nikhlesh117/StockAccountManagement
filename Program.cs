7134bea1a5835b02cc86ca96b3670e59d34e010b
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-21 18:14:20 +0530

    Added: Program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Stock Account Management");
+            string path = "D:\\Work\\Bridgelabz\\.net repo\\StockManagement\\StockManagement\\Stock.json";
+
+            FetchStock fetchStock = new FetchStock();
+
+            StockDetails data = fetchStock.Read(path);
+
+            Console.WriteLine("\nSTOCK VALUE: ");
+
+            for (int i = 0; i < data.stocks.Count; i++)
+            {
+                Console.WriteLine("\n" + data.stocks[i].Stock);
+                Console.WriteLine("shares: " + data.stocks[i].Shares);
+                Console.WriteLine("price: " + data.stocks[i].Price);
+
+                int TotalValue = data.stocks[i].Shares * data.stocks[i].Price;
+                Console.WriteLine("\nThe Total Value of {0} for {1} shares is: {2} ", data.stocks[i].Stock, data.stocks[i].Shares, TotalValue);
+                Console.WriteLine("-------------------------------");
+            }
         }
     }
 }

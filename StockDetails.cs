07b11424810794210efe9ff16fb38b36edaceddd
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-21 18:10:54 +0530

    Added: Stocks Details

---
+++ StockDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagement
+{
+    public class StockDetails
+    {
+        public List<Stocks> stocks;
+    }
+    public class Stocks
+    {
+        public string Stock;
+        public int Shares;
+        public int Price;
+    }
+}

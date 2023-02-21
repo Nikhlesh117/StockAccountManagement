b2750abb04cfbf1eb68166e87d27c942a5b8a631
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-21 18:15:22 +0530

    Added: Fetch Stocks

---
+++ FetchStock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace StockManagement
+{
+    public class FetchStock
+    {
+        public StockDetails Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<StockDetails>(json);
+
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
